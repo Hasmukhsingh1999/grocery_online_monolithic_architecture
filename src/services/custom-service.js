5e18c05c6dd7433d73e7ed82646dcd549847c6a5
Hasmukhsingh1999 <hasmukhsingh90@gmail.com>
2024-06-18 17:00:51 +0530

    ✨feature: Api create user

--- src/services/custom-service.js
+++ src/services/custom-service.js
@@ -1,3 +1,24 @@
 const { CustomerRepository } = require("../database");
-const { FormateData, GeneratePassword, GenerateSalt, GenerateSignature, ValidPassword } = require("../utils");
+const {
+  FormateData,
+  GeneratePassword,
+  GenerateSalt,
+  GenerateSignature,
+  ValidPassword,
+} = require("../utils");
+const { APIError } = require("../utils/app-errors");
 
+class CustomerService {
+  constructor() {
+    this.repository = new CustomerRepository();
+  }
+
+  async SignIn(userInputs) {
+    const { email, password } = userInputs;
+    try {
+        // const existingUser = await this.repository.
+    } catch (error) {
+        
+    }
+  }
+}
